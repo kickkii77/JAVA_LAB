38df6bdd8b7b1ce3c12ae84bda78058cfa6881c7
kickkii77 <50990190+kickkii77@users.noreply.github.com>
2019-06-22 16:03:11 +0900

    Create HomeServlet.java

---
+++ HomeServlet.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/index.bs")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public HomeServlet() {
+        super();
+       
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//WEB-INF에 있는 index.jsp파일을 화면에 표시.
+		request.getRequestDispatcher("WEB-INF/index.jsp")
+		.forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
